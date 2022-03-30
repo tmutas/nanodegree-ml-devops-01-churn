2815f1cb51638dafbcebe5cafa07b990412c346e
Timo Mutas <tmutas@users.noreply.github.com>
2022-03-30 09:36:26 +0200

    Expand tests and their logging

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -23,14 +23,14 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 
-def test_import():
+def test_import(cp_test: ch.ChurnPredictor):
     '''
     test data import
     '''
     try:
-        df = ch.import_data("./data/bank_data.csv")
+        df = cp_test.import_data(conf["bank_data_path"])
         logger.info("Testing import_data: SUCCESS")
-    except FileNotFoundError as err:
+    except FileNotFoundError:
         logger.error("Testing import_data: The file wasn't found")
         #raise err
 
@@ -39,7 +39,8 @@ def test_import():
         assert df.shape[1] > 0
     except AssertionError as err:
         logger.error(
-            "Testing import_data: The file doesn't appear to have rows and columns")
+            "Testing import_data: The file doesn't appear"
+            "to have rows and columns")
         raise err
 
     try:
@@ -51,50 +52,156 @@ def test_import():
         raise err
 
 
-def test_eda():
+def test_eda(cp_test: ch.ChurnPredictor, tmpdir):
     '''
     test perform eda function
     '''
-    df = ch.import_data("./data/bank_data.csv")
+    cp_test.import_data("./data/bank_data.csv")
 
-    with TemporaryDirectory() as tmpdir:
-        ch.perform_eda(df, tmpdir)
-        hist_cols = {
-            "Churn": "count",
-            "Customer_Age": "count",
-            "Total_Trans_Ct": "count",
-            "Marital_Status": "probability"
-        }
-        # Testing that all histplots files exists in directory
-        for col in hist_cols:
-            target = f"_histplot_{col}.png"
-            try:
-                assert any(target in fl for fl in os.listdir(tmpdir))
-                logger.info("Histplot for column %s exists", col)
-            except AssertionError:
-                logger.error("Histplot for column %s DOES NOT EXIST", col)
+    cp_test.perform_eda(tmpdir)
+    hist_cols = {
+        "Churn": "count",
+        "Customer_Age": "count",
+        "Total_Trans_Ct": "count",
+        "Marital_Status": "probability"
+    }
+    # Testing that all histplots files exists in directory
+    for col in hist_cols:
+        target = f"_histplot_{col}.png"
         try:
-            assert any("_corr_heatmap.png" in fl for fl in os.listdir(tmpdir))
-            logger.info("Correlation heatmap exists")
+            assert any(target in fl for fl in os.listdir(tmpdir))
+            logger.info(
+                "Testing perform_eda: Histplot for column %s exists", col)
         except AssertionError:
-            logger.error("Correlation Heatmap DOES NOT EXIST!")
+            logger.error(
+                "Testing perform_eda: Histplot for column %s"
+                "DOES NOT EXIST", col)
+    try:
+        assert any("_corr_heatmap.png" in fl for fl in os.listdir(tmpdir))
+        logger.info("Testing perform_eda: Correlation heatmap exists")
+    except AssertionError:
+        logger.error(
+            "Testing perform_eda: Correlation Heatmap DOES NOT EXIST!")
 
 
-def test_encoder_helper():
+def test_encoder_helper(cp_test: ch.ChurnPredictor, cat_list, response):
     '''
     test encoder helper
     '''
-    pass
+    cp_test.import_data(conf["bank_data_path"])
+
+    logger.info("Testing encoder_helper")
+
+    for col in cat_list:
+        new_col_name = f"{col}_{response}"
+        try:
+            assert col in cp_test.data.columns
+            logger.info(
+                "Testing encoder_helper: Column %s to be encoded"
+                "is present", col)
+
+        except AssertionError:
+            logger.error(
+                "Testing encoder_helper: Column %s to be encoded"
+                "is not present in data!", col)
+
+    try:
+        assert response in cp_test.data.columns
+    except AssertionError:
+        logger.error(
+            "Testing encoder_helper: Response variable %s is"
+            "not a column in data", col)
+
+    logger.info("Running encoder_helper")
+
+    cp_test.encoder_helper(cat_list)
+
+    logger.info("Encoder helper has been run. Start asserting outcome")
+    for col in cat_list:
+        new_col_name = f"{col}_{response}"
+        try:
+            assert col in cp_test.data.columns
+        except AssertionError:
+            logger.error(
+                "Testing encoder_helper: Column %s to be encoded"
+                "is not present in data!", col)
+
+        try:
+            assert new_col_name in cp_test.data.columns
+        except AssertionError:
+            logger.error("Testing encoder_helper: Column %s was"
+            "not encoded!", col)
 
 
-def test_perform_feature_engineering():
+def test_perform_feature_engineering(
+    cp_test: ch.ChurnPredictor,
+    response,
+    keep_cols,
+    test_size
+):
     '''
     test perform_feature_engineering
     '''
-    pass
+    logger.info("Testing perform_feature_engineering")
+
+    cp_test.perform_feature_engineering(
+        response,
+        keep_cols,
+        test_size
+    )
+
+    attr_to_test = [
+        "x_full",
+        "y_full",
+        "x_train",
+        "y_train",
+        "x_test",
+        "y_test"
+    ]
+
+    for attr in attr_to_test:
+        try:
+            assert hasattr(cp_test, attr)
+            logger.info("Testing feature_eng: %s is present", attr)
+        except AssertionError:
+            logger.error("Testing feature_eng: %s was not created!", attr)
+
+    x_test_len = len(cp_test.x_test)
+    x_full_len = len(cp_test.x_full)
+    x_train_len = len(cp_test.x_train)
+
+    y_test_len = len(cp_test.y_test)
+    y_full_len = len(cp_test.y_full)
+    y_train_len = len(cp_test.y_train)
+
+    try:
+        assert x_test_len == y_test_len
+        assert x_full_len == y_full_len
+        assert x_train_len == y_train_len
+        logger.info("Testing feature_eng: All x and y data sizes match")
+
+    except AssertionError:
+        logger.error("Testing feature_eng: x and y data sizes don't match!")
+
+    try:
+        assert x_test_len + x_train_len == x_full_len
+        logger.info("Testing feature_eng: Train and Test data matches full!")
+
+    except AssertionError:
+        logger.error("Testing feature_eng: Train and Test data is not full!")
+
+    min_test_len = (test_size - 0.01) * x_full_len
+    max_test_len = (test_size + 0.01) * x_full_len
+
+    try:
+        assert min_test_len < x_test_len < max_test_len
+        logger.info("Testing feature_eng: Test data matches give test_size!")
+    except AssertionError:
+        logger.error("Testing feature_eng: "
+                     "Test data set is does not correspond to test_size")
 
 
-def test_train_models():
+def test_train_models(cp_test: ch.ChurnPredictor):
     '''
     test train_models
     '''
@@ -102,10 +209,35 @@ def test_train_models():
 
 
 if __name__ == "__main__":
+    model_params = {
+        "test_size": 0.3,
+        "random_state": 42,
+        "param_grid": {
+            "n_estimators": [50, 100],
+            "max_depth": [5, 50]
+        },
+        "cv": 5
+    }
+
+    cp_main = ch.ChurnPredictor(conf, model_params)
     logger.warning("Start testing import_data")
-    test_import()
+    test_import(cp_main)
 
     logger.warning("Start testing perform_eda")
-    test_eda()
+    with TemporaryDirectory() as tmpdir_main:
+        test_eda(cp_main, tmpdir_main)
+
+    test_encoder_helper(
+        cp_main,
+        conf["columns"]["cat"],
+        conf["columns"]["response"]
+    )
+
+    test_perform_feature_engineering(
+        cp_main,
+        conf["columns"]["response"],
+        conf["columns"]["feature"],
+        0.364
+    )
 
     logger.warning("All tests passed SUCCESSFULLY!")
